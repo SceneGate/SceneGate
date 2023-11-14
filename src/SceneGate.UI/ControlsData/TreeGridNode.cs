fd5778b84552877212b0357ff720f5cfe19045dd
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-11-14 22:47:32 +0100

    :sparkles: Show format binary and length

--- src/SceneGate.UI/ControlsData/TreeGridNode.cs
+++ src/SceneGate.UI/ControlsData/TreeGridNode.cs
@@ -16,13 +16,20 @@ public partial class TreeGridNode : ObservableObject
     [ObservableProperty]
     private NodeFormatKind kind;
 
+    [ObservableProperty]
+    private string humanReadableLength;
+
+    [ObservableProperty]
+    private string formatName;
+
     public TreeGridNode(Node node)
     {
         Node = node;
-        UpdateKind();
-
         Children = new ObservableCollection<TreeGridNode>();
-        UpdateChildren();
+        formatName = string.Empty;
+        HumanReadableLength = string.Empty;
+
+        UpdateNodeInfo();
     }
 
     public Node Node { get; }
@@ -42,10 +49,76 @@ public partial class TreeGridNode : ObservableObject
     {
         _ = await Task.Run(() => Node.TransformWith(converterType)).ConfigureAwait(false);
 
-        await Dispatcher.UIThread.InvokeAsync(() => {
-            UpdateKind();
-            UpdateChildren();
-        });
+        await Dispatcher.UIThread.InvokeAsync(UpdateNodeInfo);
+    }
+
+    private void UpdateNodeInfo()
+    {
+        UpdateFormatName();
+        UpdateKind();
+        UpdateChildren();
+        UpdateLength();
+    }
+
+    private void UpdateFormatName()
+    {
+        if (Node.Format is null or NodeContainerFormat) {
+            FormatName = string.Empty;
+        } else if (Node.Format is IBinary) {
+            FormatName = ReadBinaryFormatStamp();
+        } else {
+            FormatName = Node.Format.GetType().Name;
+            if (FormatName.EndsWith("Format", StringComparison.InvariantCulture)) {
+                FormatName = FormatName[..^"Format".Length];
+            }
+        }
+    }
+
+    private string ReadBinaryFormatStamp()
+    {
+        var stream = Node.Stream;
+        if (stream is null) {
+            return string.Empty;
+        }
+
+        stream.Position = 0;
+        Span<byte> binaryStamp = stackalloc byte[4];
+        int read = stream.Read(binaryStamp);
+        if (read < 2) {
+            return "Binary";
+        }
+
+        bool isValid = true;
+        Span<char> stamp = stackalloc char[read];
+        for (int i = 0; i < read && isValid; i++) {
+            if (binaryStamp[i] is < 0x20 or > 0x7F) {
+                isValid = false;
+            } else {
+                stamp[i] = (char)binaryStamp[i];
+            }
+        }
+
+        return isValid ? $"[{new string(stamp)}]" : "Binary";
+    }
+
+    private void UpdateLength()
+    {
+        string[] Magnitudes = [string.Empty, "kB", "MB", "GB", "TB"];
+
+        var stream = Node.Stream;
+        if (stream is null) {
+            HumanReadableLength = string.Empty;
+            return;
+        }
+
+        double length = stream.Length;
+        int magnitudeIdx = 0;
+        while (length >= 1024 && magnitudeIdx < Magnitudes.Length) {
+            length /= 1024;
+            magnitudeIdx++;
+        }
+
+        HumanReadableLength = $"{length:F2} {Magnitudes[magnitudeIdx]}";
     }
 
     private void UpdateChildren()
