e53e9efc9a16cfec2a8ec6f838340a73be198fd8
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-11-14 13:20:31 +0100

    :sparkles: Implement object view with propertygrid and code editor

--- src/SceneGate.UI.Formats/Common/ObjectViewModel.cs
+++ src/SceneGate.UI.Formats/Common/ObjectViewModel.cs
@@ -1,118 +1,69 @@
 ﻿namespace SceneGate.UI.Formats.Common;
+
 using System;
 using System.Text.Json;
 using CommunityToolkit.Mvvm.ComponentModel;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
+using Yarhl.IO;
+using Yarhl.Media.Text;
 
 /// <summary>
 /// View model to display any kind of .NET object.
 /// </summary>
-public class ObjectViewModel : ObservableObject, IFormatViewModel
+public partial class ObjectViewModel : ObservableObject, IFormatViewModel
 {
-    private readonly ISerializer yamlSerializer;
-    private readonly JsonSerializerOptions jsonSerializerOptions;
-    private bool showYaml;
-    private bool showJson;
-    private bool showText;
-    private bool showPropertyGrid;
-    private object format;
-    private string text;
+    [ObservableProperty]
+    private object? model;
+
+    [ObservableProperty]
+    private string? jsonText;
+
+    [ObservableProperty]
+    private string? yamlText;
+
+    public ObjectViewModel()
+        : this(new Po())
+    {
+    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ObjectViewModel" /> class.
     /// </summary>
     public ObjectViewModel(object model)
     {
-        ShowPropertyGrid = true;
-        Text = string.Empty;
+        Model = model;
+        SetYamlText(model);
+        SetJsonText(model);
+    }
 
-        yamlSerializer = new SerializerBuilder()
+    private void SetYamlText(object model)
+    {
+        var yamlSerializer = new SerializerBuilder()
             .WithNamingConvention(PascalCaseNamingConvention.Instance)
             .ConfigureDefaultValuesHandling(DefaultValuesHandling.Preserve)
             .IgnoreFields()
             .WithIndentedSequences()
             .Build();
 
-        jsonSerializerOptions = new JsonSerializerOptions {
+        try {
+            YamlText = yamlSerializer.Serialize(model);
+        } catch (Exception ex) {
+            YamlText = $"Problem serializing as YAML:\n{ex}";
+        }
+    }
+
+    private void SetJsonText(object model)
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             WriteIndented = true,
         };
 
-        Format = model;
-    }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether it should show the object
-    /// as YAML.
-    /// </summary>
-    public bool ShowYaml {
-        get => showYaml;
-        set {
-            SetProperty(ref showYaml, value);
-            ShowText = ShowJson || value;
-        }
-    }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether it should show the object
-    /// as JSON.
-    /// </summary>
-    public bool ShowJson {
-        get => showJson;
-        set {
-            SetProperty(ref showJson, value);
-            ShowText = ShowYaml || value;
-        }
-    }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether it should show its text
-    /// representation.
-    /// </summary>
-    public bool ShowText {
-        get => showText;
-        set {
-            SetProperty(ref showText, value);
-            UpdateText();
-        }
-    }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether it should show the object
-    /// in a property grid.
-    /// </summary>
-    public bool ShowPropertyGrid {
-        get => showPropertyGrid;
-        set => SetProperty(ref showPropertyGrid, value);
-    }
-
-    /// <summary>
-    /// Gets or sets the text representation of the object.
-    /// </summary>
-    public string Text {
-        get => text;
-        set => SetProperty(ref text, value);
-    }
-
-    /// <summary>
-    /// Gets or sets the format object.
-    /// </summary>
-    public object Format {
-        get => format;
-        set => SetProperty(ref format, value);
-    }
-
-    private void UpdateText()
-    {
         try {
-            if (ShowYaml) {
-                Text = yamlSerializer.Serialize(format);
-            } else if (ShowJson) {
-                Text = JsonSerializer.Serialize(format, format.GetType(), jsonSerializerOptions);
-            }
+            JsonText = JsonSerializer.Serialize(model, jsonSerializerOptions);
         } catch (Exception ex) {
-            Text = ex.ToString();
+            JsonText = $"Problem serializing as JSON:\n{ex}";
         }
     }
 }
