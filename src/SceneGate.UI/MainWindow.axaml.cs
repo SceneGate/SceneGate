116a5e8a22fb9ac15723d7a20e6c0c7036049f81
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-12-08 12:17:08 +0100

    :sparkles: Display version metadata

--- src/SceneGate.UI/MainWindow.axaml.cs
+++ src/SceneGate.UI/MainWindow.axaml.cs
@@ -1,17 +1,27 @@
 ﻿namespace SceneGate.UI;
 
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using FluentAvalonia.UI.Windowing;
 
 public partial class MainWindow : AppWindow
 {
+    private const string DevVersion = "0.0.0-dev";
+
     public MainWindow()
     {
         InitializeComponent();
 
-        Version version = Assembly.GetExecutingAssembly().GetName().Version!;
-        string versionText = (version.Build == 0) ? "DEVELOPMENT BUILD" : $"v{version}";
+        // Get assembly with the build metadata (except commit number)
+        string assemblyPath = Assembly.GetExecutingAssembly().Location;
+        string version = FileVersionInfo.GetVersionInfo(assemblyPath).ProductVersion
+            ?? DevVersion;
+        if (version.Contains('+')) {
+            version = version[..version.IndexOf('+')];
+        }
+
+        string versionText = (version == DevVersion) ? "DEVELOPMENT BUILD" : $"v{version}";
         Title = $"SceneGate ~~ {versionText}";
     }
 }
