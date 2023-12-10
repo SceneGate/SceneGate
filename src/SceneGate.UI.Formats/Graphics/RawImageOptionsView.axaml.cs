48248f9cae3db87ee2da0bd320ae65aff7c273cd
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-12-10 21:59:32 +0100

    :sparkles: Layout to define raw image options

---
+++ src/SceneGate.UI.Formats/Graphics/RawImageOptionsView.axaml.cs
@@ -0,0 +1,39 @@
+﻿namespace SceneGate.UI.Formats.Graphics;
+
+using System.Globalization;
+using Avalonia.Controls;
+using SceneGate.UI.Formats.Controls;
+
+/// <summary>
+/// User control to define the options of a raw image.
+/// </summary>
+public partial class RawImageOptionsView : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RawImageOptionsView"/> class.
+    /// </summary>
+    public RawImageOptionsView()
+    {
+        InitializeComponent();
+    }
+
+    private void OffsetHexCheckboxChecked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var checkbox = sender as CheckBox;
+        OffsetNumericBox.TextConverter = (checkbox?.IsChecked ?? false)
+            ? new HexadecimalValueConverter()
+            : null;
+        RefreshNumericBox(OffsetNumericBox);
+    }
+
+    private void RefreshNumericBox(NumericUpDown box)
+    {
+        // Due to a bug in Avalonia, it doesn't refresh the text when the converter changes
+        // We do it manually
+        if (box.TextConverter is null) {
+            box.Text = box.Value?.ToString(box.FormatString, box.NumberFormat);
+        } else {
+            box.Text = box.TextConverter.ConvertBack(box.Value, typeof(string), null, CultureInfo.CurrentCulture)?.ToString();
+        }
+    }
+}
---
+++ src/SceneGate.UI.Formats/Graphics/RawImageOptionsViewModel.cs
@@ -0,0 +1,139 @@
+﻿namespace SceneGate.UI.Formats.Graphics;
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.Text;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Texim.Formats;
+using Texim.Images;
+using Texim.Pixels;
+using Yarhl.IO;
+
+/// <summary>
+/// View model to set and display the options of raw images.
+/// </summary>
+public partial class RawImageOptionsViewModel : ObservableObject
+{
+    private readonly IBinary binaryFormat;
+    private readonly StringBuilder hexBuilder;
+
+    [ObservableProperty]
+    private IndexedImage? image;
+
+    [ObservableProperty]
+    private long offset;
+
+    [ObservableProperty]
+    private long maximumOffset;
+
+    [ObservableProperty]
+    private int width;
+
+    [ObservableProperty]
+    private int height;
+
+    [ObservableProperty]
+    private int pixelEncoding;
+
+    [ObservableProperty]
+    private int swizzlingKind;
+
+    [ObservableProperty]
+    private bool isTiled;
+
+    [ObservableProperty]
+    private int tileWidth;
+
+    [ObservableProperty]
+    private int tileHeight;
+
+    [ObservableProperty]
+    private string hexContent;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RawImageOptionsViewModel"/> class.
+    /// </summary>
+    public RawImageOptionsViewModel()
+    {
+        Offset = 0;
+        TileWidth = 8;
+        TileHeight = 8;
+        hexContent = string.Empty;
+        hexBuilder = new StringBuilder();
+
+        if (Design.IsDesignMode) {
+            Width = 256;
+            Height = 192;
+
+            var random = new Random(42);
+            byte[] colorBytes = new byte[Width * Height];
+            random.NextBytes(colorBytes);
+            DataStream stream = DataStreamFactory.FromArray(colorBytes);
+            binaryFormat = new BinaryFormat(stream);
+
+            ReadImage();
+        } else {
+            binaryFormat = null!;
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RawImageOptionsViewModel"/> class.
+    /// </summary>
+    /// <param name="binaryFormat">The binary data to see as image.</param>
+    public RawImageOptionsViewModel(IBinary binaryFormat)
+        : this()
+    {
+        ArgumentNullException.ThrowIfNull(binaryFormat);
+
+        // TODO: guess sizes
+        this.binaryFormat = binaryFormat;
+        MaximumOffset = binaryFormat.Stream.Length;
+
+        ReadImage();
+    }
+
+    private int Size => (int)Math.Ceiling(Width * Height * Indexed4Bpp.Instance.BitsPerPixel / 8.0);
+
+    private void ReadImage()
+    {
+        ReadHexContent();
+        ConvertImage();
+    }
+
+    private void ConvertImage()
+    {
+        var options = new RawIndexedImageParams {
+            Width = Width,
+            Height = Height,
+            Offset = Offset,
+            PixelEncoding = Indexed8Bpp.Instance,
+            Size = Size,
+            Swizzling = new TileSwizzling<IndexedPixel>(new(TileWidth, TileHeight), Width),
+        };
+        Image = new RawBinary2IndexedImage(options).Convert(binaryFormat);
+    }
+
+    private void ReadHexContent()
+    {
+        binaryFormat.Stream.Position = Offset;
+        byte[] buffer = ArrayPool<byte>.Shared.Rent(Size);
+        int read = binaryFormat.Stream.Read(buffer, 0, Size);
+
+        hexBuilder.Clear();
+        for (int i = 0; i < read; i++) {
+            if (i + 1 == read) {
+                hexBuilder.AppendFormat("{0:X2}", buffer[i]);
+            } else if (i != 0 && ((i + 1) % 16 == 0)) {
+                hexBuilder.AppendFormat("{0:X2}\n", buffer[i]);
+            } else {
+                hexBuilder.AppendFormat("{0:X2} ", buffer[i]);
+            }
+        }
+
+        HexContent = hexBuilder.ToString();
+        ArrayPool<byte>.Shared.Return(buffer);
+    }
+}
