c031e3a54feae8957c63eb5f67c9602ce961624d
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-12-14 21:03:09 +0100

    :sparkles: Implement first text view

---
+++ src/SceneGate.UI.Formats/Common/TextView.axaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SceneGate.UI.Formats.Common;
+
+using System;
+using Avalonia.Controls;
+using Yarhl.IO;
+
+public partial class TextView : UserControl
+{
+    public TextView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/SceneGate.UI.Formats/Common/TextViewModel.cs
@@ -0,0 +1,67 @@
+﻿namespace SceneGate.UI.Formats.Common;
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Yarhl.IO;
+
+public partial class TextViewModel : ObservableObject, IFormatViewModel
+{
+    private const int MaxBufferLength = 100 * 1024;
+
+    private readonly Stream stream;
+    private readonly byte[] buffer;
+    private readonly int bufferLength;
+
+    private Encoding encoding;
+
+    [ObservableProperty]
+    private string text;
+
+    [ObservableProperty]
+    private string encodingName;
+
+    public TextViewModel(IBinary binary)
+    {
+        ArgumentNullException.ThrowIfNull(binary);
+
+        stream = binary.Stream;
+        bufferLength = stream.Length > MaxBufferLength ? MaxBufferLength : (int)stream.Length;
+        buffer = ArrayPool<byte>.Shared.Rent(bufferLength);
+        ReadBuffer();
+
+        text = string.Empty;
+        encodingName = "UTF-8";
+        encoding = Encoding.UTF8;
+        DecodeText();
+    }
+
+    partial void OnEncodingNameChanging(string value)
+    {
+        try {
+            encoding = Encoding.GetEncoding(
+                value,
+                EncoderFallback.ReplacementFallback,
+                DecoderFallback.ReplacementFallback);
+        } catch {
+        }
+    }
+
+    partial void OnEncodingNameChanged(string value)
+    {
+        DecodeText();
+    }
+
+    private void ReadBuffer()
+    {
+        stream.Position = 0;
+        stream.Read(buffer, 0, bufferLength);
+    }
+
+    private void DecodeText()
+    {
+        Text = encoding.GetString(buffer, 0, bufferLength);
+    }
+}
